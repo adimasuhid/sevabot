a61006b21056c0237bc47dc351d811baf185adca
Robbie Marcelo <rbmrclo@hotmail.com>
2013-10-11 19:06:56 +0800

    Setup soundcloud

--- modules/soundcloud.rb
+++ modules/soundcloud.rb
@@ -5,9 +5,9 @@ require 'rubygems'
 require 'soundcloud'
 
 # register a client with YOUR_CLIENT_ID as client_id_
-client = Soundcloud.new(:client_id => 'YOUR_CLIENT_ID')
+client = Soundcloud.new(:client_id => 'b9d4a11b8d64471b33fd8bb71d445d27') # Robbie's client id
 # get 10 hottest tracks
-tracks = client.get('/tracks', :limit => 10, :order => 'hotness')
+tracks = client.get(ARGV[0], :limit => ARGV[1], :order => 'hotness')
 # print each link
 tracks.each do |track|
   puts track.permalink_url
